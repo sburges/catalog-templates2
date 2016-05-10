8472efa0a3b0cffa7ab4b45c03ccf01d76ab5165
Craig Vyvial <cp16net@gmail.com>
2016-05-10 03:21:03 -0500

    fix and check formatting on templates (#34)

--- Makefile
+++ Makefile
@@ -7,9 +7,17 @@ help:
 	@echo
 	@echo "  test               Run the unit tests"
 
+all: tools test
+
 tools:
+	@echo "$(OK_COLOR)==> Getting required tools$(NO_COLOR)"
 	go get github.com/xeipuuv/gojsonschema
 	go get github.com/stretchr/testify
 
-test: tools
+test: tools test-format
+	@echo "$(OK_COLOR)==> Testing code$(NO_COLOR)"
 	go test ./... -v
+
+test-format:
+	@echo "$(OK_COLOR)==> Checking code with gofmt$(NO_COLOR)"
+	./scripts/testFmt.sh tests
--- tests/sdl_test.go
+++ tests/sdl_test.go
@@ -1,59 +1,59 @@
 package sdltests
 
 import (
-    "testing"
-    "github.com/stretchr/testify/assert"
-    "github.com/xeipuuv/gojsonschema"
-    "io/ioutil"
+	"github.com/stretchr/testify/assert"
+	"github.com/xeipuuv/gojsonschema"
+	"io/ioutil"
+	"testing"
 )
 
 func TestSDLsAreValid(t *testing.T) {
-    servicepath := "../services"
-    dirList, err := ioutil.ReadDir(servicepath)
-    assert.Nil(t, err, "Error listing services directory")
-    
-    //load all services 
-    for _, file := range dirList {
-        
-        //every directory should be a service
-        if file.IsDir() {
-            t.Log("Validating Service: " + file.Name())
-            innerList, innererr := ioutil.ReadDir(servicepath + "/" + file.Name())
-            assert.Nil(t, innererr, "Error listing version of service: " + file.Name())
-            
-            configfile, configfileerr := ioutil.ReadFile(servicepath + "/" + file.Name() + "/config.yml")
-            assert.Nil(t, configfileerr, "Error loading config file for service " + file.Name())
-            assert.True(t, len(configfile) > 0, "Found empty config file for service " + file.Name())
-                                
-            //subdirectories should be version of that service
-            for _, subFile := range innerList {
-                if subFile.IsDir() {
-                    t.Log("Validating Service " + file.Name() + " version " + subFile.Name())
-                    
-                    sdlfile := servicepath + "/" + file.Name() + "/" + subFile.Name() + "/sdl/sdl.json"
-                    contents, fileerr := ioutil.ReadFile(sdlfile)
-                    
-                    //sdl should exist
-                    assert.Nil(t, fileerr, "Error loading sdl file for service " + file.Name() + " version " + subFile.Name())
-                    
-                    //sdl should not be empty
-                    assert.True(t, len(contents) > 0, "Found empty sdl file for service " + file.Name() + " version " + subFile.Name())
-                    
-                    //load the sdl and validate it against the schema
-                    documentLoader := gojsonschema.NewReferenceLoader("file://" + sdlfile)
-                    schemaLoader := gojsonschema.NewReferenceLoader("file://./sdl_schema.json")
-                    
-                    result,err := gojsonschema.Validate(schemaLoader, documentLoader)
-                    assert.Nil(t, err, "There was an unexpected error")
-                    
-                    for _, err := range result.Errors() {
-                        t.Log(err)
-                    }
-        
-                    assert.Equal(t, len(result.Errors()), 0, "There was an error validating the SDL schema") 
-                }
-                
-            }
-        }
-    }
+	servicepath := "../services"
+	dirList, err := ioutil.ReadDir(servicepath)
+	assert.Nil(t, err, "Error listing services directory")
+
+	//load all services
+	for _, file := range dirList {
+
+		//every directory should be a service
+		if file.IsDir() {
+			t.Log("Validating Service: " + file.Name())
+			innerList, innererr := ioutil.ReadDir(servicepath + "/" + file.Name())
+			assert.Nil(t, innererr, "Error listing version of service: "+file.Name())
+
+			configfile, configfileerr := ioutil.ReadFile(servicepath + "/" + file.Name() + "/config.yml")
+			assert.Nil(t, configfileerr, "Error loading config file for service "+file.Name())
+			assert.True(t, len(configfile) > 0, "Found empty config file for service "+file.Name())
+
+			//subdirectories should be version of that service
+			for _, subFile := range innerList {
+				if subFile.IsDir() {
+					t.Log("Validating Service " + file.Name() + " version " + subFile.Name())
+
+					sdlfile := servicepath + "/" + file.Name() + "/" + subFile.Name() + "/sdl/sdl.json"
+					contents, fileerr := ioutil.ReadFile(sdlfile)
+
+					//sdl should exist
+					assert.Nil(t, fileerr, "Error loading sdl file for service "+file.Name()+" version "+subFile.Name())
+
+					//sdl should not be empty
+					assert.True(t, len(contents) > 0, "Found empty sdl file for service "+file.Name()+" version "+subFile.Name())
+
+					//load the sdl and validate it against the schema
+					documentLoader := gojsonschema.NewReferenceLoader("file://" + sdlfile)
+					schemaLoader := gojsonschema.NewReferenceLoader("file://./sdl_schema.json")
+
+					result, err := gojsonschema.Validate(schemaLoader, documentLoader)
+					assert.Nil(t, err, "There was an unexpected error")
+
+					for _, err := range result.Errors() {
+						t.Log(err)
+					}
+
+					assert.Equal(t, len(result.Errors()), 0, "There was an error validating the SDL schema")
+				}
+
+			}
+		}
+	}
 }
